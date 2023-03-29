4635513f5c6937297fd801e12b8605edeff9066b
Naba <naranbaatarnamsraijav@gmail.com>
2023-03-29 16:45:27 +0800

    first push

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div className='Head'>
+        <img className='imgL' src="" alt="" />
+        <button className='btns'>Home</button>
+        <button className='btns'>Service</button>
+        <button className='btns'>About</button>
+        <button className='btns'>Blog</button>
+
+        <button className='Cbtn'>Contact</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
