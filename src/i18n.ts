a2a113ce124f42b6bfe5d62a21a2006afed20ea7
Shine <batsvkhshinebayr@gmail.com>
2023-03-31 04:31:15 +0800

    first

---
+++ src/i18n.ts
@@ -0,0 +1,34 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: {
+          mtext: " technologies and innovations to market",
+          main: "A based on technological innovation to bring new"
+        },
+      },
+      mn: {
+        translation: {
+          mtext: " технологийн инновацид суурилсан",
+          main: "Шинэ технологи, шинэлэг санаачлагуудыг зах зээлд нийлүүлэх "
+        },
+      },
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+  });
+
+export default i18n;
